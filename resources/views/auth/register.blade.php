5f8b395d4f106e3a222fe3455ed9fc4075905869
Paul <ahmadzufukli@gmail.com>
2024-01-02 22:40:04 +0800

    working with admin panel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,22 +23,22 @@
 
                     <!-- Name -->
                     <x-input-label for="name" :value="__('Name')" />
-                    <x-text-input id="name" class="outline-border border-white shadow" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    <x-text-input id="name" class="outline-border border-white shadow w-96" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
 
                     <!-- Email Address -->
                     <x-input-label for="email" :value="__('Email')" />
-                    <x-text-input id="email" class="outline-none border-white shadow" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                    <x-text-input id="email" class="outline-none border-white shadow w-96" type="email" name="email" :value="old('email')" required autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
 
                     <!-- Password -->
                     <x-input-label for="password" :value="__('Password')" />
-                    <x-text-input id="password" class="outline-none border-white shadow" type="password" name="password" required autocomplete="new-password" />
+                    <x-text-input id="password" class="outline-none border-white shadow w-96" type="password" name="password" required autocomplete="new-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
 
                     <!-- Confirm Password -->
                     <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-                    <x-text-input id="password_confirmation" class="outline-none border-white shadow" type="password" name="password_confirmation" required autocomplete="new-password" />
+                    <x-text-input id="password_confirmation" class="outline-none border-white shadow w-96" type="password" name="password_confirmation" required autocomplete="new-password" />
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
 
                     <div class="flex items-center justify-between mt-4">
