4e52a880f9ae860f9c31e579c5195e84f76a8abc
Paul <ahmadzufukli@gmail.com>
2024-01-06 21:31:02 +0800

    added some styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,8 +47,8 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#999C9A" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>                                                                                    
-                    <x-side-nav-link href="{{ route('news') }}" :active="request()->routeIs('news')" class="text-lg font-medium">
-                        {{ __('Overview')}}
+                    <x-side-nav-link href="{{ route('news') }}" :active="request()->routeIs('news')" class="text-lg font-medium mt-1">
+                        {{ __('Overview')}} 
                     </x-side-nav-link>
                 </div>
                 
@@ -57,7 +57,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#999C9A" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>                                                                                                                             
-                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium">
+                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium mt-1">
                         {{ __('User')}}
                     </x-side-nav-link>
                 </div>
@@ -67,7 +67,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#999C9A" stroke-width="2" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                     </svg>                                                                                                        
-                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium">
+                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium mt-1">
                         {{ __('Manage Pets')}}
                     </x-side-nav-link>
                 </div>
@@ -77,7 +77,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#999C9A" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                     </svg>                                                                                                                             
-                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium">
+                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium mt-1">
                         {{ __('Notifications')}}
                     </x-side-nav-link>
                 </div>
@@ -87,7 +87,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#999C9A" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
                     </svg>                                                                                                                                               
-                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium">
+                    <x-side-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lg font-medium mt-1">
                         {{ __('Messages')}}
                     </x-side-nav-link>
                 </div>
